30cf078704dc9956d052277f901b60585e306cb0
Ganeshpathivada <83342048+Ganeshpathivada@users.noreply.github.com>
2021-05-18 19:22:30 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Contact from './contact';
+ReactDOM.render(
+  <div className="height ">
+  <Contact />
+  </div>,
+  document.getElementById('root')
+);
+
+
